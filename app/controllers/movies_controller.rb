f9f3a039f0e883f24a3032056943dc9b16253cc8
JWDonovan <jameswilliamdonovan@gmail.com>
2021-01-16 14:54:38 -0500

    added CRUD functions for movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,57 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update, :destroy]
+
+  # /movies
   def index
     @movies = Movie.all
   end
 
+  # /movies/:id
+  def show
+  end
+
+  # /movies/new
   def new
     @movie = Movie.new
+  end
+
+  # POST /movies
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to @movie
+    else
+      render 'new'
+    end
+  end
+
+  # /movies/:id/edit
+  def edit
+  end
+
+  # PATCH /movies/:id
+  def update
+    if @movie.update(movie_params)
+      redirect_to @movie
+    else
+      redirect_to edit_movies_path(@movie)
+    end
+  end
+
+  # DELETE /movies/:id
+  def destroy
+    @movie.destroy
+    redirect_to movies_path
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :poster, :synopsis, :release_year)
+  end
+
+  def set_movie
+    @movie = Movie.find(params[:id])
   end
 end
