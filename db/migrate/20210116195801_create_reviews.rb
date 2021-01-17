7e945f605f3c449aef6d708faea0bdada9280779
JWDonovan <jameswilliamdonovan@gmail.com>
2021-01-17 09:14:11 -0500

    created reviews table

---
+++ db/migrate/20210116195801_create_reviews.rb
@@ -0,0 +1,12 @@
+class CreateReviews < ActiveRecord::Migration[6.0]
+  def change
+    create_table :reviews do |t|
+      t.string :title,   null: false
+      t.decimal :rating, null: false
+
+      t.timestamps null: false
+    end
+
+    add_foreign_key :reviews, :movies
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_16_190829) do
+ActiveRecord::Schema.define(version: 2021_01_16_195801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,5 +53,15 @@ ActiveRecord::Schema.define(version: 2021_01_16_190829) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "title", null: false
+    t.decimal "rating", null: false
+    t.bigint "movies_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["movies_id"], name: "index_reviews_on_movies_id"
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "reviews", "movies", column: "movies_id"
 end
