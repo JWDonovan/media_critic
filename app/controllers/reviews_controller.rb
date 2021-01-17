5c12a5cf9bc97f4b35314396db5bc3d32b4baca3
JWDonovan <jameswilliamdonovan@gmail.com>
2021-01-17 09:15:57 -0500

    created reviews controller with full CRUD ops

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,58 @@
+class ReviewsController < ApplicationController
+  before_action :set_review, only: [:show, :edit, :update, :destroy]
+
+  # /reviews
+  def index
+    reviews = Review.all
+    render 'index', locals: { reviews: reviews }
+  end
+
+  # /reviews/:id
+  def show
+  end
+
+  # /reviews/new
+  def new
+    @review = Review.new
+  end
+
+  # POST /reviews
+  def create
+    @review = Review.new(review_params)
+
+    if @review.save
+      redirect_to @review
+    else
+      render 'new'
+    end
+  end
+
+  # /reviews/:id/edit
+  def edit
+  end
+
+  # PATCH /reviews/:id
+  def update
+    if @review.update(review_params)
+      redirect_to @review
+    else
+      redirect_to edit_reviews_path(@review)
+    end
+  end
+
+  # DELETE /reviews/:id
+  def destroy
+    @review.destroy
+    redirect_to reviews_path
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:title, :content, :rating, :movie_id)
+  end
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
+end
