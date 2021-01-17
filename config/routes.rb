2678868582934e9d502bfbd64ebaac652aab4c5f
JWDonovan <jameswilliamdonovan@gmail.com>
2021-01-17 11:17:08 -0500

    added nested resource routes for review

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@
 
 Rails.application.routes.draw do
   root 'movies#index'
+  
   resources :movies
   resources :reviews
+
+  resources :movies, only: [:show] do
+    resources :reviews, only: [:new, :edit, :show]
+  end
 end
