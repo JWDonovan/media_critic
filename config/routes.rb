9427635c5a020bb35450e9bdfa8efa28d2500b05
JWDonovan <jameswilliamdonovan@gmail.com>
2021-01-17 11:21:34 -0500

    rubocop linting

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@
 
 Rails.application.routes.draw do
   root 'movies#index'
-  
+
   resources :movies
   resources :reviews
 
   resources :movies, only: [:show] do
-    resources :reviews, only: [:new, :edit, :show]
+    resources :reviews, only: %i[new edit show]
   end
 end
