8bc074373827e03c0efcfc864904b8876185b929
JWDonovan <jameswilliamdonovan@gmail.com>
2021-01-17 10:32:17 -0500

    fixed movie_id foreign key reference on reviews table

--- db/migrate/20210116195801_create_reviews.rb
+++ db/migrate/20210116195801_create_reviews.rb
@@ -3,12 +3,11 @@
 class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews do |t|
-      t.string :title,   null: false
-      t.decimal :rating, null: false
+      t.string :title,     null: false
+      t.decimal :rating,   null: false
+      t.references :movie, null: false, foreign_key: true
 
       t.timestamps null: false
     end
-
-    add_foreign_key :reviews, :movies
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -56,12 +56,12 @@ ActiveRecord::Schema.define(version: 2021_01_16_195801) do
   create_table "reviews", force: :cascade do |t|
     t.string "title", null: false
     t.decimal "rating", null: false
-    t.bigint "movies_id"
+    t.bigint "movie_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["movies_id"], name: "index_reviews_on_movies_id"
+    t.index ["movie_id"], name: "index_reviews_on_movie_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "reviews", "movies", column: "movies_id"
+  add_foreign_key "reviews", "movies"
 end
