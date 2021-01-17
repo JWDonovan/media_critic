14f45c66176d5e2c35caf4eb768bc5c7b0d213e6
JWDonovan <jameswilliamdonovan@gmail.com>
2021-01-17 13:06:56 -0500

    created devise user model

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  has_many :movies, through: :reviews
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable, :recoverable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :rememberable, :validatable
+
+  validates :email, presence: true
+  validates :email, uniqueness: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
   resources :movies, only: [:show] do
     resources :reviews, only: %i[new edit show]
   end
+
+  devise_for :users
 end
