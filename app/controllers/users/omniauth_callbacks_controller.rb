d11dd76a2fc58c0cbe622576a8757b71cd8a49f1
James Donovan <jameswilliamdonovan@gmail.com>
2021-02-24 16:20:22 -0500

    rubocop linting

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,27 +1,32 @@
-class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def google_oauth2
-    user = User.from_google(params)
+# frozen_string_literal: true
 
-    if user.present?
-      sign_out_all_scopes
-      flash[:success] = t 'devise.omniauth_callbacks.success', kind: 'Google'
-      sign_in_and_redirect user, event: :authentication
-    else
-      flash[:alert] = t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized."
+module Users
+  class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+    def google_oauth2
+      user = User.from_google(params)
+
+      if user.present?
+        sign_out_all_scopes
+        flash[:success] = t 'devise.omniauth_callbacks.success', kind: 'Google'
+        sign_in_and_redirect user, event: :authentication
+      else
+        flash[:alert] =
+          t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized."
+      end
     end
-  end
 
-  private
+    private
 
-  def params
-    @params ||= {
-      uid: auth.uid,
-      email: auth.info.email,
-      provider: auth.provider
-    }
-  end
+    def params
+      @params ||= {
+        uid: auth.uid,
+        email: auth.info.email,
+        provider: auth.provider
+      }
+    end
 
-  def auth
-    @auth ||= request.env['omniauth.auth']
+    def auth
+      @auth ||= request.env['omniauth.auth']
+    end
   end
 end
