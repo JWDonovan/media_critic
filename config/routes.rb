15f425b1a0cd1cb2e841a975a650b8f441fdaf63
James Donovan <jameswilliamdonovan@gmail.com>
2021-07-12 14:25:59 -0400

    added user routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   end
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  resources :users, only: [:show, :edit, :update, :destroy]
 end
