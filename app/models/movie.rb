687b7e7762f5733fe0a5a692d3a09c6e4860b078
James Donovan <jameswilliamdonovan@gmail.com>
2021-07-12 14:19:54 -0400

    added average_rating scope method to movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,7 +3,8 @@
 class Movie < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :users, through: :reviews
-  accepts_nested_attributes_for :reviews
+  # accepts_nested_attributes_for :reviews
+  accepts_nested_attributes_for :reviews, reject_if: proc { |attributes| attributes['title'].blank? }
 
   has_rich_text :synopsis
   has_one_attached :poster
@@ -13,5 +14,9 @@ class Movie < ApplicationRecord
   # add average review method, order by highest review scope, and url to match
   def self.default_scope
     order('created_at DESC')
+  end
+
+  def average_rating
+    reviews.average(:rating).to_f.round(1)
   end
 end
